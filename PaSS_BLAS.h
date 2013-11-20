f367f894598516e3cdcd31814e6acd8127a1b453
happy8888bmy <happy8888bmy@gmail.com>
2013-11-20 22:15:32 +0800

    Update PaSS_BLAS.h

--- PaSS_BLAS.h
+++ PaSS_BLAS.h
@@ -1,39 +1,52 @@
+// PaSS_BLAS.h
+
 #include <cstdint>
 
-struct vec {		// the 
-	uint32* n;	// the length
-	double* e;	// the array of entries
+struct vec {        // the 
+    uint32_t* n;    // the length
+    double* e;      // the array of entries
 };
 
 struct mat {
-	uint32 n_row;	// the number of rows
-	uint32 n_col;	// the number of cols
-	vec** col;	// the array of columns
+    uint32_t n_row; // the number of rows
+    uint32_t n_col; // the number of cols
+    vec** col;      // the array of columns
 };
 
 // Note that *(m->col[i]->n) = m->n_row for all i, where m is a mat*
 
-vec* add(const vec* a, const vec* b);				// calculate a+b
-mat* add(const mat* a, const mat* b);				// calculate a+b
+void print(const vec* v);                                       // print v
+void print(const mat* m);                                       // print m
 
-vec* sub(const vec* a, const vec* b);				// calculate a-b
-mat* sub(const mat* a, const mat* b);				// calculate a-b
+vec* copy(const vec* m);                                        // copy v
+mat* copy(const mat* v);                                        // copy m
 
-vec* mul(const vec* v, const double e);				// calculate e*v
-mat* mul(const mat* m, const double e);				// calculate e*m
-vec* mul(const mat* m, const vec* v);				// calculate m*v
-vec* mul(const vec* v, const mat* m);				// calculate (v'*m)'
-mat* mul(const vec* v, const vec* w);				// calculate v*w'
+vec* add(const vec* a, const vec* b);                           // calculate a+b
+mat* add(const mat* a, const mat* b);                           // calculate a+b
 
-double inner(const vec* v);					// calculate sum(v.*v)
-double inner(const vec* a, const vec* b);			// calculate sum(a.*b)
-double inner(const mat* m);					// calculate sum(sum(m.*m))
-double inner(const mat* a, const mat* b);			// calculate sum(sum(a.*b))
+vec* sub(const vec* a, const vec* b);                           // calculate a-b
+mat* sub(const mat* a, const mat* b);                           // calculate a-b
 
-void extend(vec* v, const double e);				// add a new entry
-void extend_col(mat* m, const uint32 i, const uint32 j);	// add a new col
-void extend_row(mat* m, const uint32 i, const uint32 j);	// add a new row
+vec* mul(const vec* v, const double e);                         // calculate e*v
+mat* mul(const mat* m, const double e);                         // calculate e*m
+vec* mul(const mat* m, const vec* v);                           // calculate m*v
+vec* mul(const vec* v, const mat* m);                           // calculate (v'*m)' = m'v
+mat* mul(const vec* v, const vec* w);                           // calculate v*w'
 
-void permute(vec* v, const double e);				// permute two entries
-void permute_col(mat* m, const vec* v);				// permute two cols
-void permute_row(mat* m, const vec* v);				// permute two rows
+double inner(const vec* v);                                     // calculate sum(v.*v)
+double inner(const vec* a, const vec* b);                       // calculate sum(a.*b)
+double inner(const mat* m);                                     // calculate sum(sum(m.*m))
+double inner(const mat* a, const mat* b);                       // calculate sum(sum(a.*b))
+
+void insert(vec* v, const double e);                            // add a new entry at the end
+void insert_col(mat* m, const vec* v);                          // add a new col at the end
+void insert_row(mat* m, const vec* v);                          // add a new row at the end
+
+void shed(vec* v);                                              // remove the final entry
+void shed_col(mat* m);                                          // remove the final col
+void shed_row(mat* m);                                          // remove the final row
+
+void swap(vec* v, const uint32_t i, const uint32_t j);          // swap two entries
+void swap_col(mat* m, const uint32_t i, const uint32_t j);      // swap two cols
+void swap_row(mat* m, const uint32_t i, const uint32_t j);      // swap two rows
+
