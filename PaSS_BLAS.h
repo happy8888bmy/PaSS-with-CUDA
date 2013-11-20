0583fe54b5deca8990dffb9e3cdc450bcfd67c80
happy8888bmy <happy8888bmy@gmail.com>
2013-11-21 00:24:35 +0800

    Update PaSS_BLAS.h

--- PaSS_BLAS.h
+++ PaSS_BLAS.h
@@ -27,44 +27,46 @@ struct mat {
     vec** col;      // the array of columns
 };
 
-vec* construct(const uint32_t* n);                      // construct a n-by-1 vector
-mat* construct(const uint32_t* p, const uint32_t* q);   // construct a p-by-q matrix
+vec* construct(const uint32_t* n);                          // construct a n-by-1 vector
+mat* construct(const uint32_t* p, const uint32_t* q);       // construct a p-by-q matrix
 
-void destruct(const vec* v);    // destruct the vector
-void destruct(const mat* m);    // destruct the matrix
+void destruct(const vec* v);                                // destruct the vector
+void destruct(const mat* m);                                // destruct the matrix
 
-void print(const vec* v);   // display the vector
-void print(const mat* m);   // display the matrix
+void print(const vec* v);                                   // display the vector
+void print(const mat* m);                                   // display the matrix
 
-vec* copy(const vec* m);    // copy the matrix
-mat* copy(const mat* v);    // copy the matrix
+vec* copy(const vec* m);                                    // copy the vector
+mat* copy(const mat* v);                                    // copy the matrix
 
-vec* add(const vec* a, const vec* b);   // calculate a+b
-mat* add(const mat* a, const mat* b);   // calculate a+b
+vec* add(const vec* a, const vec* b);                       // calculate a+b
+mat* add(const mat* a, const mat* b);                       // calculate a+b
+void add(vec* a, const vec* b);                             // a += b
+void add(mat* a, const mat* b);                             // a += b
 
-vec* sub(const vec* a, const vec* b);   // calculate a-b
-mat* sub(const mat* a, const mat* b);   // calculate a-b
+vec* mul(const vec* v, const double d);                     // calculate d*v
+mat* mul(const mat* m, const double d);                     // calculate d*m
+vec* mul(const mat* m, const vec* v);                       // calculate m*v
+vec* mul(const vec* v, const mat* m);                       // calculate m'*v (same as (v'*m)')
+mat* mul(const vec* v, const vec* w);                       // calculate v*w'
+void mul(vec* v, const double d);                           // v *= d
+void mul(mat* m, const double d);                           // m *= d
+void mul(const mat* m, vec* v);                             // calculate v = m*v
+void mul(vec* v, const mat* m);                             // calculate v = m'*v
 
-vec* mul(const vec* v, const double e); // calculate e*v
-mat* mul(const mat* m, const double e); // calculate e*m
-vec* mul(const mat* m, const vec* v);   // calculate m*v
-vec* mul(const vec* v, const mat* m);   // calculate (v'*m)' = m'v
-mat* mul(const vec* v, const vec* w);   // calculate v*w'
+double inner(const vec* v);                                 // calculate sum(v.*v)
+double inner(const vec* a, const vec* b);                   // calculate sum(a.*b)
+double inner(const mat* m);                                 // calculate sum(sum(m.*m))
+double inner(const mat* a, const mat* b);                   // calculate sum(sum(a.*b))
 
-double inner(const vec* v);                 // calculate sum(v.*v)
-double inner(const vec* a, const vec* b);   // calculate sum(a.*b)
-double inner(const mat* m);                 // calculate sum(sum(m.*m))
-double inner(const mat* a, const mat* b);   // calculate sum(sum(a.*b))
+void insert(vec* v, const double e);                        // add a new entry at the end
+void insert_col(mat* m, const vec* v);                      // add a new col at the end
+void insert_row(mat* m, const vec* v);                      // add a new row at the end
 
-void insert(vec* v, const double e);    // add a new entry at the end
-void insert_col(mat* m, const vec* v);  // add a new col at the end
-void insert_row(mat* m, const vec* v);  // add a new row at the end
-
-void shed(vec* v);      // remove the final entry
-void shed_col(mat* m);  // remove the final col
-void shed_row(mat* m);  // remove the final row
+void shed(vec* v);                                          // remove the last entry
+void shed_col(mat* m);                                      // remove the last col
+void shed_row(mat* m);                                      // remove the last row
 
 void swap(vec* v, const uint32_t i, const uint32_t j);      // swap two entries
 void swap_col(mat* m, const uint32_t i, const uint32_t j);  // swap two cols
 void swap_row(mat* m, const uint32_t i, const uint32_t j);  // swap two rows
-
